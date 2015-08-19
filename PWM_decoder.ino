ddf776b95ed4b91eedd17598652a20bd35f73ba3
Tiryoh <tiryoh@gmail.com>
2015-08-19 18:07:11 +0900

    MAJOR UPDATE
    fix min and max of map()
    unify line feed code to LF
    update pin definition
    update Serial.printf to correspond to CSV

--- PWM_decoder.ino
+++ PWM_decoder.ino
@@ -1,25 +1,25 @@
 /*
  * This program is made for deocding PWM duty ratio.
  * Copyright (c) 2015 Tiryoh <tiryoh@gmail.com>
- * 
+ *
  * This software is released under the MIT License.
  * http://opensource.org/licenses/mit-license.php
  */
 
 //define period (ms)
-#define period 100
+#define period 20
 //define Max duty ratio (percent)
-#define DutyMax 90
+#define DutyMax 10
 //define Min. duty ratio (percent)
-#define DutyMin 10
+#define DutyMin 5
 
 //pin definition
-int rcv1 = 5; //1st ch
-int rcv2 = 6; //2nd ch
-int outPin1_1 = 7;
-int outPin1_2 = 7;
-int outPin2_1 = 7;
-int outPin2_2 = 7;
+int rcv1 = 7; //1st ch
+int rcv2 = 8; //2nd ch
+int outPin1_1 = 5;
+int outPin1_2 = 6;
+int outPin2_1 = 9;
+int outPin2_2 = 10;
 
 //variable definition
 volatile unsigned long duration1=0,duration2=0;
@@ -32,21 +32,24 @@ void setup()
   pinMode(outPin1_1, OUTPUT);
   pinMode(outPin1_2, OUTPUT);
   pinMode(outPin2_1, OUTPUT);
-	pinMode(outPin2_2, OUTPUT);
+  pinMode(outPin2_2, OUTPUT);
 }
 
 void loop()
 {
   duration1 = pulseIn(rcv1, HIGH);
-  int speedVal1 = map(duration1, period*1000*DutyMin/100, period*1000*DutyMax/100, -255, 255);
+  int speedVal1 = map(duration1, period*10*DutyMin, period*10*DutyMax, -255, 255);
   duration2 = pulseIn(rcv2, HIGH);
-  int speedVal2 = map(duration2, period*1000*DutyMin/100, period*1000*DutyMax/100, -255, 255);
-
+  int speedVal2 = map(duration2, period*10*DutyMin, period*10*DutyMax, -255, 255);
+  Serial.print("rcv1:,");
+  Serial.print(speedVal1);
+  Serial.print(",rcv2:,");
+  Serial.println(speedVal2);
   if( speedVal1 > 20)
   {
     analogWrite(outPin1_1, speedVal1);
     digitalWrite(outPin1_2, LOW);
-  } 
+  }
   else if( speedVal1 < -20)
   {
     digitalWrite(outPin1_1, LOW);
@@ -62,7 +65,7 @@ void loop()
   {
     analogWrite(outPin2_1, speedVal2);
     digitalWrite(outPin2_2, LOW);
-  } 
+  }
   else if( speedVal2 < -20)
   {
     digitalWrite(outPin2_1, LOW);
